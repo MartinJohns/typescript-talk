66cdc8f4fdef3e63160a898ecf0453fd7641ab5e
Martin Johns <martin.johns@live.de>
2019-08-20 16:10:43 +0200

    Fix compilation error after TypeScript update

--- src/08-mapped-types.ts
+++ src/08-mapped-types.ts
@@ -281,7 +281,7 @@ log(`Id: ${whoopsieMilk.id}, name: ${whoopsieMilk.name}`);
 // we can define a mapped type that will remove a specific property
 // by making clever use of intersection types and the never type,
 // or even overwriting specific properties with a new type.
-type Diff<T extends string, U extends string> = ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T];
+type Diff<T extends string | number | symbol, U extends string | number | symbol> = ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T];
 type Omit<T, K extends keyof T> = { [P in Diff<keyof T, K>]: T[P] };
 type Overwrite<T, U> = { [P in Diff<keyof T, keyof U>]: T[P] } & U;
 
